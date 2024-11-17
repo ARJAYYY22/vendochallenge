80086250cbab0c6b11694f00df02d0d274685d22
ARJAYYY22 <fajardiarjay@gmail.com>
2024-11-17 14:32:41 -0800

    display text was done!

---
+++ vendomachine.php
@@ -0,0 +1,14 @@
+!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Vendo Machine</title>
+</head>
+<body >
+  
+    <h2>Vendo Machine</h2>
+
+        <form action="" method="post">
+            <fieldset>
+                <legend><b>Drinks</b></legend>
